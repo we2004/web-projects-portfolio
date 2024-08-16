8f564a5a07dfed103c3d04203681c316c7393c2b
Wesal Esmail <113953451+we2004@users.noreply.github.com>
2024-08-16 20:45:27 +0300

    added the web projects project to the portfolio

--- projects.js
+++ projects.js
@@ -11,17 +11,23 @@ export const projects = [{
     websiteLink: 'https://we2004.github.io/Multiplication-App/',
     image: 'images/multiplicationweb.png'
 }, {
-    name: 'Sign Up Page',
-    about: 'responsive sign up page using HTML and CSS.',
-    githubLink: 'https://github.com/we2004/sign-up-page.git',
-    websiteLink: 'https://we2004.github.io/sign-up-page/',
-    image: 'images/signupweb.png'
+    name: 'Projects Portfolio',
+    about: 'A portfolio that shows a collection of web development projects that I have built.',
+    githubLink: 'https://github.com/we2004/web-projects-portfolio.git',
+    websiteLink: 'https://we2004.github.io/web-projects-portfolio/',
+    image: 'images/projectsweb.png'
 }, {
     name: 'Anime landing page',
     about: 'used bootstrap , css and html to create a simple landing page for an anime and manga website.',
     githubLink: 'https://github.com/we2004/AnimeManga-lnadingPage.git',
     websiteLink: 'https://we2004.github.io/AnimeManga-lnadingPage/',
     image: 'images/animeweb.png'
+}, {
+    name: 'Sign Up Page',
+    about: 'responsive sign up page using HTML and CSS.',
+    githubLink: 'https://github.com/we2004/sign-up-page.git',
+    websiteLink: 'https://we2004.github.io/sign-up-page/',
+    image: 'images/signupweb.png'
 }, {
     name: 'rock paper scissors',
     about: 'A simple and interactive Rock, Paper, Scissors game built with HTML, CSS, and JavaScript. The game allows users to play against the computer, with their scores being tracked and stored using localStorage, ensuring that scores persist across sessions.',
